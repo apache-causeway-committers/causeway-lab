5df7a159c2fcfb0fd8aadcdba4199fe8437fcaee
Andi Huber <ahuber@apache.org>
2022-02-09 12:55:32 +0100

    initial css grid layout stuff

---
+++ experiments/cssgridlayout/src/main/java/org/apache/isis/lab/experiments/layout/IsisLabLayoutApplication.java
@@ -0,0 +1,31 @@
+package org.apache.isis.lab.experiments.layout;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "isis-lab-experiments-layout")
+@PWA(name = "isis-lab-experiments-layout", shortName = "isis-lab-experiments-layout", offlineResources = {
+        "images/logo.png"})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class IsisLabLayoutApplication implements AppShellConfigurator {
+
+    private static final long serialVersionUID = 1L;
+
+    public static void main(final String[] args) {
+        SpringApplication.run(IsisLabLayoutApplication.class, args);
+    }
+
+}
---
+++ experiments/cssgridlayout/src/main/java/org/apache/isis/lab/experiments/layout/views/MainLayout.java
@@ -0,0 +1,115 @@
+package org.apache.isis.lab.experiments.layout.views;
+
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.Nav;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.UnorderedList;
+import com.vaadin.flow.router.RouterLink;
+
+import org.apache.isis.lab.experiments.layout.views.about.AboutView;
+import org.apache.isis.lab.experiments.layout.views.helloworld.HelloWorldView;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * A simple navigation item component, based on ListItem element.
+     */
+    public static class MenuItemInfo extends ListItem {
+
+        private static final long serialVersionUID = 1L;
+
+        private final Class<? extends Component> view;
+
+        public MenuItemInfo(final String menuTitle, final String iconClass, final Class<? extends Component> view) {
+            this.view = view;
+            RouterLink link = new RouterLink();
+            // Use Lumo classnames for various styling
+            link.addClassNames("flex", "h-m", "items-center", "px-s", "relative", "text-secondary");
+            link.setRoute(view);
+
+            Span text = new Span(menuTitle);
+            // Use Lumo classnames for various styling
+            text.addClassNames("font-medium", "text-s", "whitespace-nowrap");
+
+            link.add(new LineAwesomeIcon(iconClass), text);
+            add(link);
+        }
+
+        public Class<?> getView() {
+            return view;
+        }
+
+        /**
+         * Simple wrapper to create icons using LineAwesome iconset. See
+         * https://icons8.com/line-awesome
+         */
+        @NpmPackage(value = "line-awesome", version = "1.3.0")
+        public static class LineAwesomeIcon extends Span {
+
+            private static final long serialVersionUID = 1L;
+
+            public LineAwesomeIcon(final String lineawesomeClassnames) {
+                // Use Lumo classnames for suitable font size and margin
+                addClassNames("me-s", "text-l");
+                if (!lineawesomeClassnames.isEmpty()) {
+                    addClassNames(lineawesomeClassnames);
+                }
+            }
+        }
+
+    }
+
+    public MainLayout() {
+        addToNavbar(createHeaderContent());
+    }
+
+    private Component createHeaderContent() {
+        Header header = new Header();
+        header.addClassNames("bg-base", "border-b", "border-contrast-10", "box-border", "flex", "flex-col", "w-full");
+
+        Div layout = new Div();
+        layout.addClassNames("flex", "h-xl", "items-center", "px-l");
+
+        H1 appName = new H1("isis-lab-experiments-layout");
+        appName.addClassNames("my-0", "me-auto", "text-l");
+        layout.add(appName);
+
+        Nav nav = new Nav();
+        nav.addClassNames("flex", "gap-s", "overflow-auto", "px-m");
+
+        // Wrap the links in a list; improves accessibility
+        UnorderedList list = new UnorderedList();
+        list.addClassNames("flex", "list-none", "m-0", "p-0");
+        nav.add(list);
+
+        for (MenuItemInfo menuItem : createMenuItems()) {
+            list.add(menuItem);
+
+        }
+
+        header.add(layout, nav);
+        return header;
+    }
+
+    private MenuItemInfo[] createMenuItems() {
+        return new MenuItemInfo[]{ //
+                new MenuItemInfo("Hello World", "la la-globe", HelloWorldView.class), //
+
+                new MenuItemInfo("About", "la la-file", AboutView.class), //
+
+        };
+    }
+
+}
