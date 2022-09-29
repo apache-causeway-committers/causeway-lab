a54dc1aba0592ab54a567c8e53be1fe484b3c7e8
Andi Huber <ahuber@apache.org>
2022-09-29 07:41:17 +0200

    Bump font-awesome from 6.1.2 to 6.2.0

--- experiments/wicket-fullcal/src/main/java/org/apache/isis/lab/experiments/wktfcal/WicketFulcalApplicationConfiguration.java
+++ experiments/wicket-fullcal/src/main/java/org/apache/isis/lab/experiments/wktfcal/WicketFulcalApplicationConfiguration.java
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.isis.lab.experiments.wktfcal;
 
 import com.giffing.wicket.spring.boot.context.extensions.ApplicationInitExtension;
@@ -50,7 +68,7 @@ implements WicketApplicationInitConfiguration {
 
   static class FontAwesomeCssReferenceWkt extends WebjarsCssResourceReference {
       private static final long serialVersionUID = 1L;
-      public static final String FONTAWESOME_RESOURCE = "font-awesome/6.1.0/css/all.min.css";
+      public static final String FONTAWESOME_RESOURCE = "font-awesome/6.2.0/css/all.min.css";
 
       @Getter(lazy = true) @Accessors(fluent = true)
       private static final FontAwesomeCssReferenceWkt instance = new FontAwesomeCssReferenceWkt();
